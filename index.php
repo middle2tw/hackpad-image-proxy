59ccdb4d891a37184a7b99d2393ba3b44f36073f
Ronny Wang <ronnywang@gmail.com>
2018-01-31 11:34:14 +0800

    show unknown user message

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (!preg_match('#http://s3.amazonaws.com/hackpad-profile-photos/([^@]*@[^%?]*)#
 
 $obj = json_decode(file_get_contents('http://picasaweb.google.com/data/entry/api/user/' . $matches[1] . '?alt=json'));
 if (!$obj or !property_exists($obj->entry, 'gphoto$thumbnail') or !property_exists($obj->entry->{'gphoto$thumbnail'}, '$t')) {
-    die("unknown url, source: https://github.com/middle2tw/hackpad-image-proxy");
+    die("unknown user " . htmlspecialchars($matches[1]) . ", source: https://github.com/middle2tw/hackpad-image-proxy");
 }
 header('Content-Type: image/jpeg');
 header('Cache-Control: max-age=86400');
