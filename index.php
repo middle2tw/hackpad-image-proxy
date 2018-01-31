415d8c64f38f96b6acbfcfdc47c14b665c5a1d5e
Ronny Wang <ronnywang@gmail.com>
2018-01-31 11:05:58 +0800

    add source url

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!preg_match('#http://s3.amazonaws.com/hackpad-profile-photos/([^@]*@[^%]*)#', $_SERVER['REQUEST_URI'], $matches)) {
-    die("unknown url");
+    die("unknown url, source: https://github.com/middle2tw/hackpad-image-proxy");
 }
 
 $obj = json_decode(file_get_contents('http://picasaweb.google.com/data/entry/api/user/' . $matches[1] . '?alt=json'));
 if (!$obj or !property_exists($obj->entry, 'gphoto$thumbnail') or !property_exists($obj->entry->{'gphoto$thumbnail'}, '$t')) {
-    die("unknown url");
+    die("unknown url, source: https://github.com/middle2tw/hackpad-image-proxy");
 }
 header('Content-Type: image/jpeg');
 header('Cache-Control: max-age=86400');
